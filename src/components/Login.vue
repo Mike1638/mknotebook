98bc25dca63c5a9c49f7244c1d5429c5a6abd69d
hisoka1638 <1399952343@qq.com>
2022-04-28 23:24:26 +0800

    添加动画效果

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,23 @@
           <div class="main"></div>
           <div class="form">
             <h3 @click="showRegister">创建账户</h3>
-            <div v-show="isShowRegister" class="register">
-              <input type="text" v-model="register.name" placeholder="用户名" />
-              <input type="text" v-model="register.password" placeholder="密码" />
-              <p :class="{error:register.isError}">{{register.notice}}</p>
+            <transition name="slide">
+            <div :class="{show: isShowRegister}" class="register">
+              <input type="text" v-model="register.username" placeholder="用户名">
+              <input type="password" v-model="register.password" @keyup.enter="onRegister" placeholder="密码">
+              <p :class="{error: register.isError}"> {{register.notice}}</p>
               <div class="button" @click="onRegister">创建账号</div>
             </div>
+           </transition>
             <h3 @click="showLogin">登录</h3>
-            <div v-show="isShowLogin" class="login">
-              <input type="text" v-model="login.name"  placeholder="输入用户名" />
-              <input type="text" v-model="login.password"  placeholder="输入密码" />
-              <p :class="{error:login.isError}">{{login.notice}}</p>
-             <div class="button" @click="onLogin">登录</div>
-            </div>
+            <transition name="slide">
+              <div :class="{show: isShowLogin}" class="login">
+                <input type="text" v-model="login.username" placeholder="输入用户名">
+                <input type="password" v-model="login.password" @keyup.enter="onLogin"  placeholder="密码">
+                <p :class="{error: login.isError}"> {{login.notice}}</p>
+                <div class="button" @click="onLogin"> 登录</div>
+              </div>
+            </transition>
           </div>
         </div>
       </div>
@@ -123,8 +127,10 @@ export default {
   .form{
     width: 270px;
     border-left: 1px solid #ccc;
+    overflow: hidden;
     h3{
       padding: 10px 20px;
+      margin-top: -1px;
       font-weight: normal;
       font-size: 16px;
       border-top: 1px solid #eee;
@@ -145,8 +151,14 @@ export default {
       cursor: pointer;
     }
     .login,.register{
-      padding:10px 20px;
+      padding:0px 20px;
       border-top:1px solid #eee;
+      height:0;
+      overflow:hidden;
+      transition: height .4s;
+      &.show{
+        height: 193px;
+      }
       input{
       display:block;
       width: 100%;
@@ -157,7 +169,7 @@ export default {
       border: 1px solid #ccc;
       outline: none;
       font-size: 14px;
-      margin-top: 10px;   
+      margin-top: 10px;
       }
       input:focus{
         border: 3px solid #9dcaf8;
